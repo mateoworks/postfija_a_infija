9539c2e9fe7f6357c427e7f98557123aa5782241
mateomtz199 <mateomtz887@gmail.com>
2021-04-03 21:12:51 -0600

    Terminado

--- PostfijaAInfija/src/postfijaainfija/app/Principal.java
+++ PostfijaAInfija/src/postfijaainfija/app/Principal.java
@@ -1,6 +1,10 @@
 package postfijaainfija.app;
 class Principal {
     public static void main(String[] args) {
+        PostfijaAInfija objPostfijaAInfija = new  PostfijaAInfija();
+        System.out.println("Hola");
+        String postfija = "ab*c+";
+        System.out.println(postfija);
+        System.out.println(objPostfijaAInfija.cadenaInfija(postfija));
     }
-    
 }
