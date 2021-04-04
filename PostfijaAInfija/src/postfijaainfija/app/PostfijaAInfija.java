cbedd4555a90771d0659b5c7aed6c6da50876612
mateomtz199 <mateomtz887@gmail.com>
2021-04-03 20:55:37 -0600

    Método cadenaInfija

--- PostfijaAInfija/src/postfijaainfija/app/PostfijaAInfija.java
+++ PostfijaAInfija/src/postfijaainfija/app/PostfijaAInfija.java
@@ -1,5 +1,7 @@
 package postfijaainfija.app;
 
+import java.util.Stack;
+
 /**
  * 
  * 
@@ -13,8 +15,22 @@ public class PostfijaAInfija {
     private  boolean esUnOperador(char x){
         return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z');
     }
-    public String cadenaInfija(){
-        StackOverflowError
-        return "";
+    public String cadenaInfija(String expresionPostfija){
+        Stack<String> pila = new Stack<String>();
+        int contador = 0;
+        while(contador < expresionPostfija.length()){
+            if(esUnOperador(expresionPostfija.charAt(contador))){
+                pila.push(expresionPostfija.charAt(contador) + "");
+            }
+            else{
+                String operador1 = pila.peek();
+                pila.pop();
+                String operador2 = pila.peek();
+                pila.pop();
+                pila.push("(" + operador2 + expresionPostfija.charAt(contador) + operador1 + ")");
+            }
+            contador++;
+        }
+        return pila.peek();
     }
 }
