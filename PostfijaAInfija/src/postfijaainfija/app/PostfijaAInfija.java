714e5538fddbeca94db8dcce736f08176b0fa2a3
mateomtz199 <mateomtz887@gmail.com>
2021-04-03 20:48:48 -0600

    Metodo esUnOperador

--- PostfijaAInfija/src/postfijaainfija/app/PostfijaAInfija.java
+++ PostfijaAInfija/src/postfijaainfija/app/PostfijaAInfija.java
@@ -5,5 +5,16 @@ package postfijaainfija.app;
  * 
  */
 public class PostfijaAInfija {
-    
+    /**
+     * Comprobar si el caracter es un operador (a, b, c ... ) o no
+     * @param x valor a verficar
+     * @return true si es un operador, false si no lo es
+     */
+    private  boolean esUnOperador(char x){
+        return (x >= 'a' && x <= 'z') || (x >= 'A' && x <= 'Z');
+    }
+    public String cadenaInfija(){
+        StackOverflowError
+        return "";
+    }
 }
